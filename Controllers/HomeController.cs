4c00203cbf7e5296f646702d92f246cb67312b17
Lee, Jung Geun <junggelee@ebay.com>
2017-06-27 08:34:11 +0900

    96p Name 만

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,5 +14,20 @@ namespace LanguageFeatures.Controllers
         {
 			return "Navigate to a URL to show an example";
         }
+
+		public ViewResult AutoProperty()
+		{
+			// 새로운 product  개체를 생성한다.
+			Product myProduct = new Product();
+
+			// 속성값을 설정한다.
+			myProduct.Name = "Kayak";
+
+			// 속성값을 가져온다.
+			string productName = myProduct.Name;
+
+			// 뷰를 생성한다.
+			return View("Result", (object)String.Format("Product name: {0}", productName));
+		}
     }
 }
